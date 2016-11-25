53b827557ac47d032e10154a30b178e84041479e
noppanutn <noppanutn@gmail.com>
2016-11-25 18:00:45 +0700

    initialize

---
+++ Korn-creation/checklogin.php
@@ -0,0 +1,43 @@
+<?php
+  session_start(); // To use SESSION !!! Must be at the top of the page
+  require_once('connect.php');
+
+  $re_user = $_POST['username'];
+  $re_pass = $_POST['passwd'];
+  //$re_pass = md5($re_pass);
+
+// For better security
+
+  $q = "SELECT USER_ID, USER_PASSWD FROM staff
+  WHERE USER_ID = '".$mysqli->real_escape_string($re_user)."'
+  AND USER_PASSWD = '".$mysqli->real_escape_string($re_pass)."';
+
+  //echo $q;
+
+  $result = $mysqli->query($q);
+  if($result)
+  {
+    $count_no_row = $result->num_rows;
+    if($count_no_row == 1)
+    {
+      echo "Login Success";
+      // Keep data in SESSION
+      $row = $result->fetch_array();
+      $_SESSION['user'] = $row;
+    //  $_SESSION['user_fullname'] = $row['USER_FNAME'];
+    //  $_SESSION['user_username'] = $row['USER_NAME'];
+      $_SESSION['group'] = $row['USER_GROUPID'];
+      if($_SESSION['group']==1 || $_SESSION['group']==3){
+        header("Location: user.php");
+      } else {
+        header("Location: group.php");
+      }
+
+    } else {
+      echo "Wrong username or password";
+    }
+  } else {
+      echo "Wrong username or password!";
+  }
+
+?>
---
+++ checklogin.php
@@ -0,0 +1,46 @@
+<?php
+  session_start(); // To use SESSION !!! Must be at the top of the page
+  require_once('connect.php');
+
+  $re_user = $_POST['username'];
+  $re_pass = $_POST['passwd'];
+
+  //$q = "SELECT * FROM user WHERE username = '$re_user'
+  //AND passwd = '$re_pass' AND disable = 0; ";
+
+// For better security
+
+  $q = "SELECT * FROM staff
+  WHERE USER_USERNAME = '".$mysqli->real_escape_string($re_user)."'
+  AND USER_PASSWD = '".$mysqli->real_escape_string($re_pass)."'
+  ";//AND disable = 0; ";
+
+  echo $q;
+
+  $result = $mysqli->query($q);
+  if($result)
+  {
+    $count_no_row = $result->num_rows;
+    if($count_no_row == 1)
+    {
+      echo "Login Success";
+      // Keep data in SESSION
+      $row = $result->fetch_array();
+      $_SESSION['u_id'] = $row['USER_ID'];
+      $_SESSION['u_fullname'] = $row['USER_FNAME']." ".$row['USER_LNAME'];
+      $_SESSION['u_username'] = $row['USER_USERNAME'];
+
+      echo "<br>".$_SESSION['u_id'];
+      echo "<br>".$_SESSION['u_fullname'];
+      echo "<br>".$_SESSION['u_username'];
+
+      header("Location: sm_pinfo.php");
+
+    } else {
+      echo "Wrong username or password";
+    }
+  } else {
+      echo "Wrong username or password!";
+  }
+
+?>
